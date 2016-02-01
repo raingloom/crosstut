d05f415da65aa3aad91ab0df9c1cc8f14e813703
raingloom <raingloom42@gmail.com>
2016-02-01 10:52:36 +0000

    emscripten fixes in cpp source

--- Cross.cpp
+++ Cross.cpp
@@ -1,13 +1,31 @@
 #include <SDL2/SDL.h>
+#ifdef __EMSCRIPTEN__
+	#include <emscripten.h>
+#endif
+
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+SDL_Texture *texture;
+SDL_Event event;
+
+void main_loop()
+{
+    while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-    SDL_Event event;
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 3;
@@ -30,16 +48,12 @@ int main(int argc, char *argv[])
     }
     SDL_FreeSurface(surface);
 
-    while (1) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_QUIT) {
-            break;
-        }
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-    }
+	#ifdef __EMSCRIPTEN__
+		emscripten_set_main_loop( main_loop, 0, 0 );
+	#else
+		//TODO: proper framerate for native build
+		while(1)main_loop();
+	#endif
 
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 A simple project to test cross platform compilation.
+emcc --preload-file ../assets@/assets -s USE_SDL=2 ../Cross.cpp -o cross.html
