c7d96e81ec9319af5863df0f057b9231cf0e549b
raingloom <raingloom42@gmail.com>
2016-02-06 20:42:41 +0000

    cym completion setu pprogress

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,2 +1,17 @@
 import os
-compilation_database_folder = os.path.dirname(os.path.abspath(__file__)) + "/build"
+import shutil
+
+cwd = os.getcwd()
+jsonname='compile_commands.json'
+bjsonp=os.path.join(cwd,'build',jsonname)
+ljsonp=os.path.join(cwd,jsonname)
+
+#copies database to cwd
+if os.path.isfile(bjsonp):
+    if os.path.isfile(ljsonp):
+        if os.stat(bjsonp).st_mtime > os.stat(ljsonp).st_mtime:
+            shutil.copy( bjsonp, ljsonp )
+    else
+        shutil.copy( bjsonp, ljsonp )
+
+compilation_database_folder = cwd
